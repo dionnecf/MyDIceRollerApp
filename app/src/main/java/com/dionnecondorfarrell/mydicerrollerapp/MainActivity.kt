a5b53a6c10d213ac72c3c32e7525699ef6d387cf
Dionne <dionne.condorfarrell@tfl.gov.uk>
2019-11-11 22:54:22 +0000

    Updates from 1.3 add randomDiceImage generator

--- app/src/main/java/com/dionnecondorfarrell/mydicerrollerapp/MainActivity.kt
+++ app/src/main/java/com/dionnecondorfarrell/mydicerrollerapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dionnecondorfarrell.mydicerrollerapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,9 +11,13 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
 
@@ -21,24 +26,29 @@ class MainActivity : AppCompatActivity() {
         val countUp: Button = findViewById(R.id.count_up)
 
         countUp.setOnClickListener { countUp() }
-
     }
 
     private fun rollDice() {
 
-        val resultText: TextView = findViewById(R.id.result_text)
-
         val randomNumber = Random.nextInt(6) + 1
+        val drawableResource = when(randomNumber) {
 
-        resultText.text = randomNumber.toString()
-        Toast.makeText(this,"Dice Rolled", Toast.LENGTH_SHORT).show()
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 
     private fun countUp() {
 
-        val resultText: TextView = findViewById(R.id.result_text)
+        //val resultText: TextView = findViewById(R.id.result_text)
 
-        if (resultText.text.toString() == "Hello World!") {
+       /* if (resultText.text.toString() == "Hello World!") {
             resultText.text = "1"
         } else {
             var resultInt: Int = resultText.text.toString().toInt()
@@ -48,6 +58,6 @@ class MainActivity : AppCompatActivity() {
                 resultText.text = resultInt.toString()
             }
 
-        }
+        }*/
     }
 }
